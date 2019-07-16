7cd25a0f2521c4a445c772d24315607173d58fa2
tbrangman <tbrangma@purdue.edu>
2019-07-16 12:18:39 -0500

    hover project states

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
 
-	/* Profile/menu open and close */
-	$('.profile-btn').click(function() {
+	/* Menu open/close */
+	$('.profile-btn').click(function(){
 		$('.profile').css('display','block');
 	});
 
-	$('.exit-menu').click(function() {
+	$('.exit-menu').click(function(){
 		$('.profile').css('display','none');
 	});
+
+	/* Hover state for project/titles */
+	var tooltip = document.querySelectorAll('.projectTooltip');
+
+	document.addEventListener('mousemove', fn, false);
+
+	function fn(e) {
+	    for (var i=tooltip.length; i--;) {
+	        tooltip[i].style.left = e.pageX + -35 + 'px';
+	        tooltip[i].style.top = e.pageY + -90 + 'px';
+	    }
+	}
 
 });
